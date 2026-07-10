520e225f25391a90119051bf06794c150eac522b
nnixaa <4dmitr@gmail.com>
2026-07-10 16:12:26 +0200

    fix(worker): update image security dependencies

--- packages/worker/scripts/build-railpack.sh
+++ packages/worker/scripts/build-railpack.sh
@@ -8,8 +8,8 @@ set -eu
 git clone --depth 1 --branch "${COMPARTMENT_RAILPACK_VERSION}" https://github.com/railwayapp/railpack.git .
 # Keep these overrides until the selected Railpack release embeds fixed vulnerable Go dependencies.
 go get \
-  github.com/containerd/containerd/v2@v2.2.4 \
-  golang.org/x/crypto@v0.52.0 \
+  github.com/containerd/containerd/v2@v2.2.5 \
+  golang.org/x/crypto@v0.53.0 \
   golang.org/x/net@v0.55.0 \
   go.opentelemetry.io/otel@v1.43.0 \
   go.opentelemetry.io/otel/exporters/otlp/otlptrace@v1.43.0 \
